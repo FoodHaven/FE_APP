66463a96787cf37b8de9c14b815f4c83f1aa2d09
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-28 17:56:28 -0600

    build: add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   serialize :favorites, Array
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   def self.from_omniauth(response)
     User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |user|
