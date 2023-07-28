92c6995d2103fb5bb10ed221407029e343654e83
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-28 17:27:49 -0600

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_182636) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_230144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_182636) do
     t.string "email"
     t.string "uid"
     t.string "provider"
+    t.text "favorites"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
