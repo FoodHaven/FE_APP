eb9b7a022dc14388b30f627f33e280a7bab0fd7c
Jesse Sorman <jessesorman@gmail.com>
2023-07-28 19:07:55 -0400

    feat user can logout

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
   
   get "markets/search", to: "markets#search"
   
---
+++ spec/features/sessions/destoy_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "logout", type: :feature do
+  it "can logout" do
+    visit root_path
+    click_on "Register for new account"
+    expect(current_path).to eq(new_user_path)
+
+    fill_in :name, with: "Bob"
+    fill_in :email, with: "Bob@email.com"
+    fill_in :password, with: "1234"
+    fill_in :password_confirmation, with: "1234"
+
+    click_on "Create Account"
+    visit root_path
+    click_on "Login"
+    fill_in :email, with: "Bob@email.com"
+    fill_in :password, with: "1234"
+    click_on "Login"
+
+    click_on "Logout"
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Login")
+    expect(page).to have_content("Register for new account")
+  end
+end
