c17232d01e75313f8c82bbf3e5d5c7004c5a956a
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-29 15:17:26 -0600

    Add find_market to service file

--- app/services/market_service.rb
+++ app/services/market_service.rb
@@ -7,15 +7,6 @@ class MarketService
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
-class MarketService
-  def conn
-    Faraday.new(url: "https://foodhaven-be.onrender.com")
-  end
-
-  def get_url(url)
-    response = conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)
-  end
 
   def all_markets
     get_url("/api/v1/markets")
