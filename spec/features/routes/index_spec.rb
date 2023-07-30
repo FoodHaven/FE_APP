b7d7f3483eb64bba20ab9053ddcc930120016436
Jesse Sorman <jessesorman@gmail.com>
2023-07-30 17:14:36 -0400

    refactor stub request test for routes

--- spec/features/routes/index_spec.rb
+++ spec/features/routes/index_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe "Routes" do
     destination_lon = -73.87400881765025
     original_lat = 40.748594902847515
     original_lon =  -73.98564294463469
-    request = stub_request(:get, "http://localhost:3001/api/v1/markets/2/transit_routes?fromPlace=#{original_lat},#{original_lon}&toPlace=#{destination_lat},#{destination_lon}")
+    id = 2212
+    request = stub_request(:get, "https://foodhaven-be.onrender.com/api/v1/markets/#{id}/transit_routes?original_lat=#{original_lat}&original_lon=#{original_lon}&destination_lat=#{destination_lat}&destination_lon=#{destination_lon}")
     routes = JSON.parse(json_response, symbolize_names: true)[:data]
     expect(routes).to be_a(Array)
   end
-end k
+end
