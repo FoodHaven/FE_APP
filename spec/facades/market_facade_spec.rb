d05c8ee47889088a8acd88826e8b9dfd5775bdab
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-31 15:40:52 -0600

    test: add tests for market_facade

---
+++ spec/facades/market_facade_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe RouteFacade, :vcr do
+  let(:params) do
+    {
+      latitude: "latitude",
+      longitude: "longitude",
+      radius: "radius",
+      id: "1",
+      market_id: "1"
+    }
+  end
+
+  subject { MarketFacade.new(params) }
+
+  describe '#all_markets' do
+    it 'returns all markets' do
+      markets = subject.all_markets
+
+      expect(markets).to all(be_a(Market))
+    end
+  end
+
+  describe '#market' do
+    it 'returns a single market' do
+      market = subject.market
+
+      expect(market).to be_a(SingleMarket)
+    end
+  end
+end
