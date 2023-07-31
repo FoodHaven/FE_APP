444b67254505b1bfede06be7a8f7cffb3a0d8d65
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-31 14:59:23 -0600

    test: update stub request, now passing

--- spec/features/routes/index_spec.rb
+++ spec/features/routes/index_spec.rb
@@ -1,14 +1,21 @@
-require "rails_helper"
+require 'rails_helper'
+
 RSpec.describe "Routes" do
-  it "can find routes for market" do
-    json_response = File.read('spec/fixtures/transit_search.json')
+
+  it "can find routes for market", :vcr do
     destination_lat = 40.77709768562554
     destination_lon = -73.87400881765025
     original_lat = 40.748594902847515
-    original_lon =  -73.98564294463469
+    original_lon = -73.98564294463469
     id = 2212
-    request = stub_request(:get, "https://foodhaven-be.onrender.com/api/v1/markets/#{id}/transit_routes?original_lat=#{original_lat}&original_lon=#{original_lon}&destination_lat=#{destination_lat}&destination_lon=#{destination_lon}")
-    routes = JSON.parse(json_response, symbolize_names: true)[:data]
-    expect(routes).to be_a(Array)
+
+    response = RouteService.new.all_routes(id, original_lat, original_lon, destination_lat, destination_lon)
+
+    expect(response).to be_an(Array)
+    expect(response[0]).to have_key(:agency_name)
+    expect(response[0]).to have_key(:route_short_name)
+    expect(response[0]).to have_key(:headsign)
+    expect(response[0]).to have_key(:start_time)
+    expect(response[0]).to have_key(:end_time)
   end
 end
