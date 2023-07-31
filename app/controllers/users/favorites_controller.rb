50968442331753192543b6fed998c0dbaf348605
Jesse Sorman <jessesorman@gmail.com>
2023-07-31 17:33:52 -0400

    removes flash alert for must be signed in to add/remove market for favorites

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -12,8 +12,6 @@ class Users::FavoritesController < ApplicationController
     current_user.favorites << @market[:data][:id].to_i
     if current_user.save
       flash[:success] = 'Market added to favorites!'
-    else
-      flash[:alert] = 'Failed to add market to favorites.'
     end
     redirect_to market_path(@market[:data][:id])
   end
@@ -24,8 +22,6 @@ class Users::FavoritesController < ApplicationController
     current_user.favorites.delete(@market[:data][:id].to_i)
     if current_user.save
       flash[:success] = 'Market removed from favorites.'
-    else
-      flash[:alert] = 'Failed to remove market from favorites.'
     end
     redirect_to market_path(@market[:data][:id])
   end
