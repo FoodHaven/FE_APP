85c2421f2c748c0500d96a8cf89da86c60e2e488
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-11 13:54:04 -0600

    test: update test for route service

--- spec/services/route_service_spec.rb
+++ spec/services/route_service_spec.rb
@@ -1,42 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe RouteService do
-  let(:route_service) { RouteService.new }
-
   describe '#all_routes' do
-    it 'fetches all transit routes for a market ID and coordinates' do
-      market_id = 1
-      original_lat = 40.7128
-      original_lon = -74.0060
-      destination_lat = 37.7749
-      destination_lon = -122.4194
+    it 'fetches all routes', :vcr do
+      service = RouteService.new
+      id = 1
+      original_lat = 40.0150
+      original_lon = -105.2705
+      destination_lat = 39.8367
+      destination_lon = -105.0372
 
+      response = service.all_routes(original_lat, original_lon, destination_lat, destination_lon)
 
-      stub_request(:get, "https://foodhaven-be.onrender.com/api/v1/markets/#{market_id}/transit_routes?original_lat=#{original_lat}&original_lon=#{original_lon}&destination_lat=#{destination_lat}&destination_lon=#{destination_lon}")
-        .to_return(status: 200, body: '[{"name": "Route 1"}, {"name": "Route 2"}]', headers: { 'Content-Type' => 'application/json' })
-
-      response = route_service.all_routes(market_id, original_lat, original_lon, destination_lat, destination_lon)
-
-      expect(response).to be_an(Array)
-      expect(response.size).to eq(2)
-      expect(response[0][:name]).to eq('Route 1')
-      expect(response[1][:name]).to eq('Route 2')
+      expect(response).to be_a(Hash)
+      expect(response[:plan]).to be_a(Hash)
+      expect(response[:plan][:itineraries]).to be_an(Array)
     end
   end
-  
+
   describe "#route_details" do
-    it "gets details of a route" do
-      json_response = File.read('spec/fixtures/route_details.json')
+    it "gets details of a route", :vcr do
+      route_service = RouteService.new
 
-      id = "MTABC:33875"
-      stub_request(:get, "https://foodhaven-be.onrender.com/api/v1/route_details?global_route_id=#{id}").to_return(status: 200, body: json_response, headers: {})
+      id = "RTD:62467"
+
       response = route_service.one_route(id)
-      routes = JSON.parse(json_response, symbolize_names: true)
 
-      expect(response).to be_an(Hash)
-      expect(response[:data][0][:attributes][:stops][0][:stop_name]).to eq("Roosevelt Av / 61 St")
-      expect(response[:data][0][:attributes][:stops][0][:stop_lat]).to eq(40.74567731104018)
-      expect(response[:data][0][:attributes][:stops][0][:stop_lon]).to eq(-73.90210722249947)
+      expect(response).to be_a(Hash)
+      expect(response[:itineraries][0][:stops][0][:stop_name]).to eq("Downtown Boulder Station Gate 1")
     end
   end
 end
