0c6cfd5fee632567341656cc8a7edfc05ae5baa7
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-11 13:03:28 -0600

    refact: add vcr filtering of api key

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,5 +76,6 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data('transit_api_key') { ENV['transit_api_key'] }
   config.allow_http_connections_when_no_cassette = true
 end
