790d49677092f67d53236a6c5a0891f3a474eee6
Jesse Sorman <jessesorman@gmail.com>
2023-08-12 16:41:30 -0400

    google maps set up

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     patch 'add_to_favorites', to: 'users/favorites#create', on: :member
     delete 'remove_from_favorites', to: 'users/favorites#destroy', on: :member
     resources :users, only: [:show]
-    resources :routes, only: [:index, :show]
+    resources :commutes, only: [:index]
   end
   resources :users, only: [:new, :create]
 
