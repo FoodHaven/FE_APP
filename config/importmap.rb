3fbd6db221d2db6a54c77a3a20dc72c1a8bad983
Jesse Sorman <jessesorman@gmail.com>
2023-08-14 18:26:15 -0400

    deletes unused files

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loding.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/custom", under: "custom"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
